0d0b75d944aab76b1b47cfd37d09f21f6f2944c2
Stuart Hoffman <stu@hoffmanswebsite.com>
2016-02-18 15:19:37 -0800

    First commit of project

--- OldTimeMac/ViewController.swift
+++ OldTimeMac/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLbl: UILabel!
+    
+    var phrases = ["Booting from floppy...\n", "Reading from disk...\n","Updating Registry...\n", "......\n",
+    ".............\n",".............................\n", "Welcome, Jessica\n it is nice to see you again\n"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mainLbl.text = ""
+        for var x = 0; x < phrases.count; x++
+        {
+            var txt = mainLbl.text!
+            txt += phrases[x]
+            mainLbl.text = txt
+        }
     }
 
     override func didReceiveMemoryWarning() {
